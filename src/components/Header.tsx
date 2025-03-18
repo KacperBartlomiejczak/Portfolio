f7993cd335f4b7ab4b4c3d5a89025e105807a64c
Kacper Bartlomiejczak <bartlomiejczakkapi@gmail.com>
2025-03-19 00:43:06 +0100

    second commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
       <div className="container mx-auto mt-4 md:mt-2 md:px-8 py-2 md:py-0">
         <div className="flex justify-center flex-col items-center h-full md:flex-row">
           <img
-            src="src\assets\images\logo.jpg"
+            src="public/logo.jpg"
             alt="A boy in suit with beautiful smile and with blond curly hairs"
             className="w-70 h-70 object-cover rounded-full mb-5 border-5 border-primary hover:shadow-lg hover:shadow-primary transition-shadow duration-300"
           />
