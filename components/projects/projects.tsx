c88299e686f853058018a7e6e983c1f9fd2123de
KacperBartlomiejczak <kacperbartlomiejczak@proton.me>
2025-10-24 18:02:22 +0200

    added some small changes

--- components/projects/projects.tsx
+++ components/projects/projects.tsx
@@ -6,7 +6,9 @@ export default function Projects() {
     <section className="flex items-center p-4 justify-center" id="projects">
       <div className="container flex flex-col items-start gap-4">
         <SectionHeder>Projekty</SectionHeder>
-        <div className="flex flex-col items-center justifty-center w-full my-2">
+        <div className="flex flex-row flex-wrap items-center justify-center w-full my-2 gap-4 md:my-4 lg:my-8 md:gap-6 lg:gap-8">
+          <ProjectCard />
+          <ProjectCard />
           <ProjectCard />
         </div>
       </div>
