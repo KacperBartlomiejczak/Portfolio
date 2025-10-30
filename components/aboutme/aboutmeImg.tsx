a7dcd6772aadfc4767d5d2bcf963df5267146fe5
Kacperbartlomiejczak <kacperbartlomiejczak@proton.me>
2025-10-30 13:55:05 +0100

    changed image path for about me

--- components/aboutme/aboutmeImg.tsx
+++ components/aboutme/aboutmeImg.tsx
@@ -1,7 +1,7 @@
 export default function AboutMeImg() {
   return (
     <img
-      src="/aboutMeImg.jpg"
+      src="/aboutmeImg.jpg"
       alt="Chłopak z koniem na kiju"
       className="block w-52 h-52 rounded-full object-cover object-center hover:ring-2 focus:ring-2 ring-primary-color ring-offset-3 transition-all duration-300 md:w-72 md:h-72 lg:w-80 lg:h-80 xl:w-90 xl:h-90 hover:scale-110 "
     />
