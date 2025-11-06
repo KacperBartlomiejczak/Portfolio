f1cec9ba3ec4d768fdc8707a3d0148635b4f5005
Kacperbartlomiejczak <kacperbartlomiejczak@proton.me>
2025-11-06 11:22:50 +0100

    Added more faster loading of fonts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -3,6 +3,13 @@ import { Inter, Fira_Code } from "next/font/google";
 export const inter = Inter({
   subsets: ["latin"],
   weight: ["400", "600", "700", "800"],
+  display: "swap",
+  variable: "--font-inter",
 });
 
-export const firaCode = Fira_Code({ subsets: ["latin"], weight: ["400"] });
+export const firaCode = Fira_Code({
+  subsets: ["latin"],
+  weight: ["400"],
+  display: "swap",
+  variable: "--font-fira-code",
+});
