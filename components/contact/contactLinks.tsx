a3900f5f90553a15c846f6e80ce114aea44db0ee
KacperBartlomiejczak <kacperbartlomiejczak@proton.me>
2025-11-07 10:59:02 +0100

    Changed fonts for specyfic paragraphs

--- components/contact/contactLinks.tsx
+++ components/contact/contactLinks.tsx
@@ -7,6 +7,7 @@ import {
 } from "@/public/svg/logos";
 
 import ContactLink from "./contactLink";
+import { firaCode } from "@/app/ui/fonts";
 
 export default function ContactLinks() {
   return (
@@ -14,7 +15,7 @@ export default function ContactLinks() {
       <a
         href="mailto:kacperbartlomiejczak@proton.me"
         aria-label="Wyślij do mnia maila"
-        className="relative border-2 border-primary-color text-primary-color w-full p-2 rounded-lg flex flex-row justify-between items-center cursor-pointer md:w-[40%] overflow-hidden before:content-[''] before:absolute before:inset-0 before:bg-primary-color before:scale-x-0 before:origin-left before:transition-transform before:duration-300 hover:before:scale-x-100 hover:before:origin-right hover:text-white before:z-0 z-10 transition-colors delay-250 dark:border-brand dark:before:bg-brand dark:text-brand dark:hover:text-white"
+        className={`relative border-2 border-primary-color text-primary-color w-full p-2 rounded-lg flex flex-row justify-between items-center cursor-pointer md:w-[40%] overflow-hidden before:content-[''] before:absolute before:inset-0 before:bg-primary-color before:scale-x-0 before:origin-left before:transition-transform before:duration-300 hover:before:scale-x-100 hover:before:origin-right hover:text-white before:z-0 z-10 transition-colors delay-250 dark:border-brand dark:before:bg-brand dark:text-brand dark:hover:text-white ${firaCode.className}`}
         target="_blank"
       >
         <div className="h-full flex flex-row items-center gap-4 z-10">
