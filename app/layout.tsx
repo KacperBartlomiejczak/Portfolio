7477a06dd17e903d5ad5b63286a1cb81a1643379
KacperBartlomiejczak <kacperbartlomiejczak@proton.me>
2025-11-07 10:56:16 +0100

    Added icon for my website

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,13 +72,14 @@ export const metadata: Metadata = {
 
   icons: {
     icon: [
-      { url: "/favicon-32.png", sizes: "32x32", type: "image/png" },
-      { url: "/favicon-192.png", sizes: "192x192", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-64x64.png", sizes: "64x64", type: "image/png" },
     ],
     apple: [
-      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+      { url: "/apple-touch-icon.png", sizes: "64x64", type: "image/png" },
     ],
-    shortcut: ["/favicon.ico"],
+    shortcut: ["/favicon.png"],
   },
 
   verification: {
